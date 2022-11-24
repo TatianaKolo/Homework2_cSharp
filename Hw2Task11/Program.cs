9706b06771fedcc6ee4a0da045be6585fdc80d2d
TatianaKolo <manila-nick@yandex.ru>
2022-11-24 19:26:46 +0300

    task11 is ready

--- Hw2Task11/Program.cs
+++ Hw2Task11/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 11 Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+
+int Number = new Random().Next(100, 999);
+int NewNumber = 0;
+int NumberWithoutSecDig()
+{
+    NewNumber = Number / 100 * 10 + Number % 10;
+    return NewNumber;
+}
+NumberWithoutSecDig();
+System.Console.WriteLine();
+System.Console.WriteLine($"Если из числа {Number} удалить вторую цифру, получится число {NewNumber}");
+System.Console.WriteLine();
