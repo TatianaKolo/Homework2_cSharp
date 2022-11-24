640d2ae6252d67c25b4def021376c6fdd127184f
TatianaKolo <manila-nick@yandex.ru>
2022-11-24 19:26:18 +0300

    task10 is ready

--- Hw2Task1/Program.cs
+++ Hw2Task1/Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
 
 int number = new Random().Next(100, 999);
 
